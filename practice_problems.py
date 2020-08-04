118aa3d45bfb86ef47bfa2b9cbf46d5d24a8a498
Hayden Bazor <hbazor@murraystate.edu>
2020-08-04 18:06:00 -0500

    calculates the area of a circle

---
+++ practice_problems.py
@@ -0,0 +1,6 @@
+from math import pi
+
+"""This calculates the area of a circle."""
+r = float(input("Input the radius of the circle: "))
+
+print("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
