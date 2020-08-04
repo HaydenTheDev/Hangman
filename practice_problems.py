876a1283df9fc8409369d56053579c119d8631e4
Hayden Bazor <hbazor@murraystate.edu>
2020-08-04 18:16:23 -0500

    input name and print it backwards with a space.

--- practice_problems.py
+++ practice_problems.py
@@ -1,6 +1,16 @@
 from math import pi
 
-"""This calculates the area of a circle."""
-r = float(input("Input the radius of the circle: "))
+# """This calculates the area of a circle."""
+# r = float(input("Enter the radius for the circle here: "))
+#
+# print("The area of the circle with a radius of " + str(r) + " is " + str(pi * r**2))
 
-print("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+# """Input name and print it reversed with a space in between"""
+#
+# first_name = input("Please enter your first name: ")
+# last_name = input("Please enter your last name: ")
+#
+# print(last_name + " " + first_name)
+
+
