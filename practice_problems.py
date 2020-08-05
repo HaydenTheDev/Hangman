6de76218e3365092d9b3eafe4e734747802a82d0
Hayden Bazor <hbazor@murraystate.edu>
2020-08-05 12:38:50 -0500

    Write a Python program to test whether a number  is within 100 of 1000 or 2000.

--- practice_problems.py
+++ practice_problems.py
@@ -118,3 +118,14 @@ from math import pi
 # else:
 #     print("nevermind")
 
+"""Write a Python program to test whether a number
+ is within 100 of 1000 or 2000."""
+
+answer = int(input("enter an number close to 1000 or 2000: "))
+
+
+def near_thousand(n):
+    return (abs(1000 - n) <= 100) or (abs(2000 - n) <= 100)
+
+
+print(near_thousand(answer))
