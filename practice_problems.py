f672699e2b484e6b98e8e7a41b941105c267a207
Hayden Bazor <hbazor@murraystate.edu>
2020-08-04 20:56:04 -0500

    Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.

--- practice_problems.py
+++ practice_problems.py
@@ -42,11 +42,23 @@ from math import pi
 # print(l_name + " " + f_name)
 
 
-"""Write a Python program to 
-display the examination schedule. 
-(extract the date from exam_st_date)"""
+# """Write a Python program to
+# display the examination schedule.
+# (extract the date from exam_st_date)"""
+#
+# exam_st_date = (11, 12, 2014)
+#
+#
+# print("The examination will start from: %i/%i/%i"%exam_st_date)
 
-exam_st_date = (11, 12, 2014)
+"""Write a Python program that accepts an integer (n) and 
+computes the value of n+nn+nnn."""
 
+n = int(input("Enter a number: "))
 
-print("The examination will start from: %i/%i/%i"%exam_st_date)
+n1 = int("%s" % n)
+n2 = int("%s%s" % (n, n))
+n3 = int("%s%s%s" % (n, n, n))
+
+calculate = n1 + n2 + n3
+print(calculate)
