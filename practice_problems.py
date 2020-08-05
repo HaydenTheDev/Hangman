f54f2f548647418442375a38d5dc99adaf1bab5a
Hayden Bazor <hbazor@murraystate.edu>
2020-08-05 12:59:45 -0500

    Write a Python program to calculate the sum of three given numbers,  if the values are equal then return three times of their sum.

--- practice_problems.py
+++ practice_problems.py
@@ -1,19 +1,19 @@
 from math import pi
 
-# """This calculates the area of a circle."""
+"""This calculates the area of a circle."""
 # r = float(input("Enter the radius for the circle here: "))
 #
 # print("The area of the circle with a radius of " + str(r) + " is " + str(pi * r**2))
 
 
-# """Input name and print it reversed with a space in between"""
+"""Input name and print it reversed with a space in between"""
 #
 # first_name = input("Please enter your first name: ")
 # last_name = input("Please enter your last name: ")
 #
 # print(last_name + " " + first_name)
 
-# """Create an input with 5 comma separated number and make them into a list and
+"""Create an input with 5 comma separated number and make them into a list and
 # a tuple."""
 # values = input("Please input 4 values separated by commas: ")
 #
@@ -23,8 +23,8 @@ from math import pi
 # print('List : ', list)
 # print('Tuple : ', tuple)
 
-# """Write a Python program to accept a filename
-# from the user and print the extension of that"""
+"""Write a Python program to accept a filename
+from the user and print the extension of that"""
 #
 # filename = input("Please enter the filename that you want: ")
 #
@@ -33,8 +33,8 @@ from math import pi
 # print("The extension of the file is " + repr(file_ext[-1]))
 
 
-# """Write a Python program to display the
-# first and last colors from the following list."""
+"""Write a Python program to display the
+first and last colors from the following list."""
 #
 # f_name = input("Please enter your first name: ")
 # l_name = input("Please enter your last name: ")
@@ -42,9 +42,9 @@ from math import pi
 # print(l_name + " " + f_name)
 
 
-# """Write a Python program to
-# display the examination schedule.
-# (extract the date from exam_st_date)"""
+"""Write a Python program to
+display the examination schedule.
+(extract the date from exam_st_date)"""
 #
 # exam_st_date = (11, 12, 2014)
 #
@@ -52,8 +52,8 @@ from math import pi
 # print("The examination will start from: %i/%i/%i"%exam_st_date)
 
 
-# """Write a Python program that accepts an integer (n) and
-# computes the value of n+nn+nnn."""
+"""Write a Python program that accepts an integer (n) and
+ computes the value of n+nn+nnn."""
 #
 # n = int(input("Enter a number: "))
 #
@@ -65,15 +65,15 @@ from math import pi
 # print(calculate)
 
 
-# """Write a Python program to print the documents (syntax, description
-# etc.) of Python built-in function(s)."""
+"""Write a Python program to print the documents (syntax, description
+ etc.) of Python built-in function(s)."""
 #
 # print(abs.__doc__)
 # print(repr.__doc__)
 # print(next.__doc__)
 
-# """Write a Python program to print the calendar of a given month and year.
-# Note : Use 'calendar' module."""
+"""Write a Python program to print the calendar of a given month and year.
+Note : Use 'calendar' module."""
 #
 # import calendar
 #
@@ -86,7 +86,7 @@ from math import pi
 
 # its a doc string like above.
 
-# """Write a Python program to calculate number of days between two dates."""
+"""Write a Python program to calculate number of days between two dates."""
 # import datetime
 #
 # date_one = datetime.date(1995, 5, 6)
@@ -96,7 +96,7 @@ from math import pi
 #
 # print(difference)
 
-# """Write a Python program to get the volume of a sphere with radius 6."""
+"""Write a Python program to get the volume of a sphere with radius 6."""
 # import math
 #
 # radius = float(input("Enter the radius of the sphere: "))
@@ -106,8 +106,8 @@ from math import pi
 # volume = float(4/3)*pi*cube
 # print("The volume of the this sphere is: {}".format(volume))
 
-# """Write a Python program to get the difference between a given number and 17,
-#  if the number is greater than 17 return double the absolute difference. """
+"""Write a Python program to get the difference between a given number and 17,
+ if the number is greater than 17 return double the absolute difference. """
 #
 # number_one = int(input("Enter a number: "))
 #
@@ -120,12 +120,26 @@ from math import pi
 
 """Write a Python program to test whether a number
  is within 100 of 1000 or 2000."""
+#
+# answer = int(input("enter an number close to 1000 or 2000: "))
+#
+#
+# def near_thousand(n):
+#     return (abs(1000 - n) <= 100) or (abs(2000 - n) <= 100)
+#
+#
+# print(near_thousand(answer))
 
-answer = int(input("enter an number close to 1000 or 2000: "))
+"""Write a Python program to calculate the sum of three given numbers,
+ if the values are equal then return three times of their sum. """
 
+number_1 = float(input("Please enter the first number: "))
+number_2 = float(input("Please enter the second number: "))
+number_3 = float(input("Please enter the third number: "))
 
-def near_thousand(n):
-    return (abs(1000 - n) <= 100) or (abs(2000 - n) <= 100)
+number_sum = number_1 + number_2 + number_3
 
-
-print(near_thousand(answer))
+if number_1 == number_2 == number_3:
+    print("These numbers cubed is : {}".format(number_sum ** 3))
+else:
+    print("The sum of these numbers is: {}".format(number_sum))
