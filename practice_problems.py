6f99adf6335f6cfb8ef3f4c54e109f2c50cdd3b3
Hayden Bazor <hbazor@murraystate.edu>
2020-08-05 19:50:52 -0500

    Write a Python program to test whether a passed letter is a vowel or not.

--- practice_problems.py
+++ practice_problems.py
@@ -209,17 +209,34 @@ copies of the first 2 characters of a given string.
 Return the n copies of the whole string if the length is less than 2."""
 
 
-def substring_copy(str, n):
-    flen = 2
-    if flen > len(str):
-        flen = len(str)
-    substr = str[:flen]
+# def substring_copy(str, n):
+#     flen = 2
+#     if flen > len(str):
+#         flen = len(str)
+#     substr = str[:flen]
+#
+#     result = ""
+#     for i in range(n):
+#         result = result + substr
+#     return result
+#
+#
+# print(substring_copy('abcdef', 2))
+# print(substring_copy('p', 3))
 
-    result = ""
-    for i in range(n):
-        result = result + substr
-    return result
+"""Write a Python program to test whether 
+a passed letter is a vowel or not. """
 
 
-print(substring_copy('abcdef', 2))
-print(substring_copy('p', 3));
+user_input = input("Please enter a letter: ")
+
+
+def vowel_or_not(letter):
+    vowel = "aeiou"
+    if letter in vowel:
+        print("This is a vowel")
+    else:
+        print("This is a consonant")
+
+
+vowel_or_not(user_input)
