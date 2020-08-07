ec6b2cb33ac2995e391211814c20bb25a0210b91
Hayden Bazor <hbazor@murraystate.edu>
2020-08-06 20:01:04 -0500

    Write a Python program to print out a set containing all the  colors from color_list_1 which are not present in color_list_2.

--- practice_problems.py
+++ practice_problems.py
@@ -294,20 +294,30 @@ in a list into a string and return it."""
  numbers that come after 237 in the sequence."""
 
 
-def ordered_numbers(n):
-    for numbers in n:
-        if numbers % 2 == 0:
-            print(numbers)
-        elif numbers == 237:
-            print(numbers)
-            break
-    return
+# def ordered_numbers(n):
+#     for numbers in n:
+#         if numbers % 2 == 0:
+#             print(numbers)
+#         elif numbers == 237:
+#             print(numbers)
+#             break
+#     return
+#
+#
+# print(ordered_numbers([386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+#                        399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+#                        815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+#                        958, 743, 527]))
 
 
-print(ordered_numbers([386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
-                       399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
-                       815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
-                       958,743, 527]))
+"""Write a Python program to print out a set containing all the
+ colors from color_list_1 which are not present in color_list_2."""
+
+
+list_one = {"Halo", "Fallout", "RocketLeague", "OverWatch", "WarZone", "WoW"}
+list_two = {"Halo", "Fallout", "RocketLeague", "OverWatch"}
+
+print(list_one.difference(list_two))
 
 
 
