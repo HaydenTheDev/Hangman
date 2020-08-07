57c62f3a8fc5191632fa083e5a83bdacc0fa7b08
Hayden Bazor <hbazor@murraystate.edu>
2020-08-07 17:13:56 -0500

    find the sum of the n positive integers

--- practice_problems.py
+++ practice_problems.py
@@ -527,9 +527,14 @@ platform and release information"""
 
 
 """Write a Python to find local IP addresses using Python's stdlib"""
-import socket
-print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
-    if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
-        s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
-            socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+# import socket
+# print([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+#     if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
+#         s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
+#             socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 
+
+"""Write a python program to find the sum of the first n positive integers."""
+n = int(input("Input a number: "))
+sum_num = (n * (n + 1)) / 2
+print(sum_num)
