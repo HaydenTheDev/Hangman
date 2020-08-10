e170bcfe042d008050e0c925634c77987e7ed99f
Hayden Bazor <hbazor@murraystate.edu>
2020-08-10 11:52:17 -0500

    Write a Python program to get a directory listing,  sorted by creation date.

--- practice_problems.py
+++ practice_problems.py
@@ -666,15 +666,33 @@ distance (in feet) to inches, yards, and miles."""
 
 
 """Write a Python program to sort files by date."""
-import glob
-import os
+# import glob
+# import os
+#
+# new_file = open("bobs_burger.txt", 'w')
+# new_file.write("This show is alright.")
+# new_file.close()
+#
+#
+# files = glob.glob("*.txt")
+# files.sort(key=os.path.getmtime)
+# print("\n".join(files))
 
-new_file = open("bobs_burger.txt", 'w')
-new_file.write("This show is alright.")
-new_file.close()
+
+"""Write a Python program to get a directory listing,
+ sorted by creation date."""
+
+from stat import S_ISREG, ST_CTIME, ST_MODE
+import os, sys, time
 
 
-files = glob.glob("*.txt")
-files.sort(key=os.path.getmtime)
-print("\n".join(files))
+dir_path = sys.argv[1] if len(sys.argv) == 2 else r'.'
+
+data = (os.path.join(dir_path,fn) for fn in os.listdir(dir_path))
+data = ((os.stat(path), path) for path in data)
+
+data = ((stat[ST_CTIME], path) for stat, path in data if S_ISREG(stat[ST_MODE]))
+
+for cdate, path in sorted(data):
+    print(time.ctime(cdate), os.path.basename(path))
 
