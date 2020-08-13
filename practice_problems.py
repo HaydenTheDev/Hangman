7cd884b29dfa4ce339148b4ca2c231b3bfdc4639
Hayden Bazor <hbazor@murraystate.edu>
2020-08-13 16:56:54 -0500

    Write a Python program to split a variable length string into variables.

--- practice_problems.py
+++ practice_problems.py
@@ -986,9 +986,20 @@ largest and smallest integers, longs, floats. """
  multiple variables have the same value. """
 
 
-x = 20
-y = 20
-z = 20
+# x = 20
+# y = 20
+# z = 20
+#
+# if x == y == z == 20:
+#     print("All the values are equal.")
 
-if x == y == z == 20:
-    print("All the values are equal.")
+
+"""Write a Python program to split a variable length string into variables."""
+
+
+var_list = ['a', 'b', 'c']
+x, y, z = (var_list + [None] * 3)[:3]
+print(x, y, z)
+var_list = [100, 20.25]
+x, y = (var_list + [None] * 2)[:2]
+print(x, y)
